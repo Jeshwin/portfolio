02fbbd783cac6ee7ac0f0a95202a41e609aeac4e
Jeshwin Prince <jeshwinjprince@gmail.com>
2023-02-07 19:05:30 -0800

    use jsx components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,12 +10,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className='min-h-screen flex flex-row justify-between items-center'>
-          <div className="hero min-h-screen" style={{ backgroundImage: `url("https://picsum.photos/1920/1080")` }}>
+          <div className="hero min-h-screen bg-cover" style={{ backgroundImage: `url("https://images.unsplash.com/photo-1544646290-9ec4c8ff8277?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")` }}>
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-md">
                     <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <p className="mb-5">
+                      Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
                     <button className="btn btn-primary">Get Started</button>
                 </div>
             </div>
