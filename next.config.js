02d5a65c8d8743e5487843f2c20bb0255a6e2818
Jeshwin Prince <jeshwinjprince@gmail.com>
2023-02-09 18:28:18 -0800

    add MDX

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
+// next.config.js
+
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Configure pageExtensions to include md and mdx
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  // Optionally, add any other Next.js config below
   reactStrictMode: true,
+  experimental: {
+    mdxRs: true,
+  }
 }
 
-module.exports = nextConfig
+// Merge MDX config with Next.js config
+module.exports = withMDX(nextConfig)
