5680a9b8189475e28dcc388a3c300a765f348cf4
Jeshwin Prince <jeshwinjprince@gmail.com>
2023-02-11 14:32:37 -0800

    cosmetics

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import ThemeToggle from "./themetoggle"
 
 export default function Navbar() {
     return (
-        <div className="navbar xl:px-48 bg-base-200 sticky z-50 top-0">
+        <div className="navbar lg:px-40 xl:px-80 bg-base-200 sticky z-50 top-0">
           <div className="flex-none">
             <label htmlFor="my-drawer" className="btn btn-ghost btn-circle drawer-button">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
