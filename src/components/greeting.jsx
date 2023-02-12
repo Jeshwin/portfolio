6787d229952dea0b8e99edada3dbd3141f8140cd
Jeshwin Prince <jeshwinjprince@gmail.com>
2023-02-11 22:52:38 -0800

    no more gradient button component

--- src/components/greeting.jsx
+++ src/components/greeting.jsx
@@ -2,30 +2,23 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Astroholder from '../../public/astroholder.png'
 
-import GradientButton from './gradientbutton'
-
 export default function Greeting() {
   return (
     <>
       <main className='min-h-screen flex flex-col lg:flex-row justify-between items-center'>
           <div className="hero min-h-screen bg-gradient-to-t from-base-100 to-base-200">
-            <div className="hero-content max-w-full w-auto p-0 lg:p-1 lg:px-40 xl:px-80 flex-col-reverse lg:flex-row">
-                <div className="mx-auto lg:mr-20 max-w-xs lg:max-w-3xl text-center text-base-content">
+            <div className="hero-content gap-6 max-w-full w-auto p-0 lg:p-1 lg:px-20 xl:px-40 flex-col-reverse lg:flex-row">
+                <div className="mx-auto max-w-xs md:max-w-xl lg:max-w-2xl text-center text-base-content">
                   <h1 className="mb-5 text-xl md:text-3xl lg:text-5xl font-bold">Hello, I&apos;m <span className="text-primary">Jeshwin Prince</span></h1>
                   <p className="mb-5 md:lext-lg lg:text-2xl">
                     I am a first-year Computer Engineering student at Santa Clara University.
                     I love programming, 3d printing, playing the guitar, and making art in Blender!
                     Check out my portfolio, resume, and blog here!
                   </p>
-                  {/* <Link href="/about" className="btn btn-primary border-0 bg-gradient-to-r from-primary to-secondary hover:from-primary-focus hover:to-secondary-focus 2xl:btn-lg 2xl:text-xl">Let&apos;s Go!</Link> */}
-                  <GradientButton href="/about" text="About Me" left="primary" right="secondary" /> 
+                  <Link href="/about" className="btn btn-primary lg:btn-lg lg:text-xl border-0 bg-gradient-to-br from-primary to-secondary hover:from-primary-focus hover:to-secondary-focus">Let&apos;s Go!</Link>
                 </div>
                 <div className="relative -mt-24 w-80 lg:w-[40rem] aspect-square">
-                <Image 
-                  src={Astroholder}
-                  alt="placeholder image of an astronaut"
-                  fill
-                />
+                  <Image fill src={Astroholder} alt="placeholder image of an astronaut" />
                 </div>
             </div>
           </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,34 +1,35 @@
+import Link from 'next/link'
+
 import MyHead from '@/components/head'
 import Greeting from '@/components/greeting'
-import HomeCard from '@/components/homecards'
 
 export default function Home() {
   return (
     <>
       <MyHead title="Hello Jeshwin" />
       <Greeting />
-      <div className='flex flex-col lg:flex-row p-4 xl:py-12 lg:px-40 xl:px-80 gap-4 w-auto'>
-        <HomeCard
-          color1="primary"
-          color2="secondary"
-          title="Portfolio"
-          description="Check out all my projects"
-          link="/projects"
-        />
-        <HomeCard
-          color1="secondary"
-          color2="accent"
-          title="Resume"
-          description="Look at a copy of my resume"
-          link="/resume"
-        />
-        <HomeCard
-          color1="accent"
-          color2="primary"
-          title="Blog"
-          description="Read my weekly blog posts"
-          link="/articles"
-        />
+      <div className='flex flex-col p-4 xl:py-12 lg:px-40 xl:px-80 gap-4 w-auto'>
+        <div className="h-auto p-8 basis-1/3 card bg-base-200 rounded-box">
+          <div className='pb-3 mb-3 text-4xl font-bold border-b-2 border-base-content'>Portfolio</div>
+          <div className='w-5/6 text-xl pb-4'>Check out all my projects</div>
+          <div className='card-actions justify-end'>
+            <Link href="/projects" className="btn lg:btn-lg btn-primary border-0 bg-gradient-to-br from-primary to-secondary hover:from-primary-focus hover:to-secondary-focus">Portfolio</Link>
+          </div>
+        </div>
+        <div className="h-auto p-8 basis-1/3 card bg-base-200 rounded-box">
+          <div className='pb-3 mb-3 text-4xl font-bold border-b-2 border-base-content'>Resume</div>
+          <div className='w-5/6 text-xl pb-4'>Look at a copy of my resume</div>
+          <div className='card-actions justify-end'>
+            <Link href="/resume" className="btn lg:btn-lg btn-secondary border-0 bg-gradient-to-br from-secondary to-accent hover:from-secondary-focus hover:to-accent-focus">Resume</Link>
+          </div>
+        </div>
+        <div className="h-auto p-8 basis-1/3 card bg-base-200 rounded-box">
+          <div className='pb-3 mb-3 text-4xl font-bold border-b-2 border-base-content'>Blog</div>
+          <div className='w-5/6 text-xl pb-4'>Read my weekly blog posts</div>
+          <div className='card-actions justify-end'>
+            <Link href="/articles" className="btn lg:btn-lg btn-accent border-0 bg-gradient-to-br from-accent to-primary hover:from-accent-focus hover:to-primary-focus">Blog</Link>
+          </div>
+        </div>
       </div>
     </>
   )
