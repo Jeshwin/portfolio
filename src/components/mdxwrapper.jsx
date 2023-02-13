1a40b67842a0938fe6bc9a7424a0368a05a2aa8f
Jeshwin Prince <jeshwinjprince@gmail.com>
2023-02-12 22:45:14 -0800

    better markdown x prose

--- src/components/mdxwrapper.jsx
+++ src/components/mdxwrapper.jsx
@@ -1,10 +1,10 @@
 export default function MdxWrapper({ meta, children }) {
   return (
     <>
-      <div className="p-5 lg:px-40 xl:px-80 lg:py-10 xl:py-20">
+      <div className="p-5 lg:px-48 xl:px-96 lg:py-10 xl:py-20">
         <h1 className="font-bold w-fit text-6xl lg:text-8xl mt-20 mb-6">{meta.title}</h1>
         <time className="text-accent text-xl italic">{meta.date}</time>
-        <div className="prose-lg lg:prose-xl mt-10">
+        <div className="max-w-full prose lg:prose-xl dark:prose-invert mt-10">
           {children}
         </div>
       </div>
